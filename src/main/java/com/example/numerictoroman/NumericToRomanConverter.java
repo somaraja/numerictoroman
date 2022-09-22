c431a2d851a1575e90aa6e87e307cd0bb4f88ae0
Somaraja <somaraja@gmail.com>
2022-09-22 22:26:46 +0100

    some more test cases added along with roman numbers

--- src/main/java/com/example/numerictoroman/NumericToRomanConverter.java
+++ src/main/java/com/example/numerictoroman/NumericToRomanConverter.java
@@ -1,10 +1,26 @@
 package com.example.numerictoroman;
 
 public class NumericToRomanConverter {
-    public String convertToRoman(Integer number) {
+
+    private NumericToRomanConverter() {}
+
+    public static String convertToRoman(int number) {
         if (number < 1) {
             return "Number should be one or more";
+        } else if (number == 1000) {
+            return "M";
+        } else if (number == 500) {
+            return "D";
+        } else if (number == 100) {
+            return "C";
+        } else if (number == 50) {
+            return "L";
+        } else if (number == 10) {
+            return "X";
+        } else if (number == 5) {
+            return "V";
+        } else {
+            return "I";
         }
-        return null;
     }
 }
--- src/test/java/com/example/numerictoroman/NumericToRomanConverterTest.java
+++ src/test/java/com/example/numerictoroman/NumericToRomanConverterTest.java
@@ -2,24 +2,51 @@ package com.example.numerictoroman;
 
 import static org.junit.Assert.assertEquals;
 
-import org.junit.Before;
 import org.junit.Test;
 
 public class NumericToRomanConverterTest {
-    NumericToRomanConverter numericToRomanConverter;
-
-    @Before
-    public void eachTest() {
-        numericToRomanConverter = new NumericToRomanConverter();
+    @Test
+    public void testIsNumberZero() {
+        assertEquals("Number should be one or more", NumericToRomanConverter.convertToRoman(0));
     }
 
     @Test
-    public void testIsNumberIsZero() {
-        assertEquals(numericToRomanConverter.convertToRoman(0), "Number should be one or more");
+    public void testIsNumberLessThanZero() {
+        assertEquals("Number should be one or more", NumericToRomanConverter.convertToRoman(0));
     }
 
     @Test
-    public void testIsNumberIsLessThanZero() {
-        assertEquals(numericToRomanConverter.convertToRoman(0), "Number should be one or more");
+    public void testConvert1ToRoman() {
+        assertEquals("I", NumericToRomanConverter.convertToRoman(1));
+    }
+
+    @Test
+    public void testConvert5ToRoman() {
+        assertEquals("V", NumericToRomanConverter.convertToRoman(5));
+    }
+
+    @Test
+    public void testConvert10ToRoman() {
+        assertEquals("X", NumericToRomanConverter.convertToRoman(10));
+    }
+
+    @Test
+    public void testConvert50ToRoman() {
+        assertEquals("L", NumericToRomanConverter.convertToRoman(50));
+    }
+
+    @Test
+    public void testConvert100ToRoman() {
+        assertEquals("C", NumericToRomanConverter.convertToRoman(100));
+    }
+
+    @Test
+    public void testConvert500ToRoman() {
+        assertEquals("D", NumericToRomanConverter.convertToRoman(500));
+    }
+
+    @Test
+    public void testConvert1000ToRoman() {
+        assertEquals("M", NumericToRomanConverter.convertToRoman(1000));
     }
 }

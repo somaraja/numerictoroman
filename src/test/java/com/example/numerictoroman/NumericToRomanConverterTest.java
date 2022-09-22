a559d98126c4f4446782f1610815c32916d7ca6b
Somaraja <somaraja@gmail.com>
2022-09-22 21:08:05 +0100

    First commit, TDD approach

---
+++ src/test/java/com/example/numerictoroman/NumericToRomanConverterTest.java
@@ -0,0 +1,13 @@
+package com.example.numerictoroman;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class NumericToRomanConverterTest {
+    NumericToRomanConverter numericToRomanConverter;
+
+    @Before
+    public void eachTest() {
+        numericToRomanConverter = new NumericToRomanConverter();
+    }
+}

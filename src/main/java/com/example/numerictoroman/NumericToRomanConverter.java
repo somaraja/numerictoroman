ad5f43fb87478d577a6b0a707d19e8a6a6657027
Somaraja <somaraja@gmail.com>
2022-09-22 22:07:20 +0100

    initial test cases added along with source class

---
+++ src/main/java/com/example/numerictoroman/NumericToRomanConverter.java
@@ -0,0 +1,10 @@
+package com.example.numerictoroman;
+
+public class NumericToRomanConverter {
+    public String convertToRoman(Integer number) {
+        if (number < 1) {
+            return "Number should be one or more";
+        }
+        return null;
+    }
+}
--- src/test/java/com/example/numerictoroman/NumericToRomanConverterTest.java
+++ src/test/java/com/example/numerictoroman/NumericToRomanConverterTest.java
@@ -1,5 +1,7 @@
 package com.example.numerictoroman;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Before;
 import org.junit.Test;
 
@@ -9,5 +11,15 @@ public class NumericToRomanConverterTest {
     @Before
     public void eachTest() {
         numericToRomanConverter = new NumericToRomanConverter();
+    }
+
+    @Test
+    public void testIsNumberIsZero() {
+        assertEquals(numericToRomanConverter.convertToRoman(0), "Number should be one or more");
+    }
+
+    @Test
+    public void testIsNumberIsLessThanZero() {
+        assertEquals(numericToRomanConverter.convertToRoman(0), "Number should be one or more");
     }
 }
